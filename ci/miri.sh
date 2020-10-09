bb2a86459387faa64ef8f9fde5e60eac8e05431e
Taiki Endo <te316e89@gmail.com>
2020-10-09 23:40:03 +0900

    disable miri on windows

--- ci/miri.sh
+++ ci/miri.sh
@@ -4,6 +4,10 @@ set -ex
 
 export RUSTFLAGS="-D warnings"
 
+if [[ "$OSTYPE" != "linux"* ]]; then
+    exit 0
+fi
+
 MIRI_NIGHTLY=nightly-$(curl -s https://rust-lang.github.io/rustup-components-history/x86_64-unknown-linux-gnu/miri)
 rustup set profile minimal
 rustup update "$MIRI_NIGHTLY"
